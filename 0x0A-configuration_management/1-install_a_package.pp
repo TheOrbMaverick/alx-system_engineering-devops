0dc87deecf9f82bec0b2d20be0d54d51dfe15a18
Ita Enang <enangita@gmail.com>
2024-03-22 11:32:42 -0700

    rope

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Puppet Manifest: Install Flask version 2.1.0 using pip3
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install python packages':
+     command   => 'pip3 install flask',
+     ensure   => '2.1.0'
 }
 
