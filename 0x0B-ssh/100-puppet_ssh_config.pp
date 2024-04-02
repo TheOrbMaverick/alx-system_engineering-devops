8926cca6ca4ef5b01a68a487c84deaee497e77d5
Ita Enang <enangita@gmail.com>
2024-04-02 15:53:55 -0700

    recalling

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,25 +1,13 @@
-# Define SSH client configuration file path
-$file_path = '/etc/ssh/ssh_config'
-
-# Ensure SSH client configuration file has correct permissions
-file { $file_path:
-  owner => 'root',
-  group => 'root',
-  mode  => '0644',
-}
-
-# Ensure SSH client uses the private key ~/.ssh/school
-file_line { 'Declare identity file':
-  path    => $file_path,
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^#?IdentityFile',
-  require => File[$file_path],
-}
-
-# Ensure SSH client refuses to authenticate using a password
-file_line { 'Turn off passwd auth':
-  path    => $file_path,
-  line    => 'PasswordAuthentication no',
-  match   => '^#?PasswordAuthentication',
-  require => File[$file_path],
+file { '/home/ubuntu/.ssh/config':
+  ensure  => 'file',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => "
+Host ubuntu@35.174.200.243
+    HostName 35.174.200.243
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+",
 }
