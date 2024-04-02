b9ebdd548909d214b7af7a49b4343a063d95e4ab
Ita Enang <enangita@gmail.com>
2024-04-02 15:59:31 -0700

    gi

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,26 @@
-file { '/home/ubuntu/.ssh/config':
-  ensure  => 'file',
+# Ensure SSH client configuration directory exists
+file { '/home/ubuntu/.ssh':
+  ensure  => directory,
+  mode    => '0700',
   owner   => 'ubuntu',
   group   => 'ubuntu',
+}
+
+# Create SSH client configuration file
+file { '/home/ubuntu/.ssh/config':
+  ensure  => present,
   mode    => '0600',
-  content => "
-Host ubuntu@35.174.200.243
-    HostName 35.174.200.243
-    User ubuntu
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-",
+  content => "\
+Host <YOUR_SERVER_IP>
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no\n",
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+}
+
+# Restart SSH service after making changes
+service { 'ssh':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/home/ubuntu/.ssh/config'],
 }
