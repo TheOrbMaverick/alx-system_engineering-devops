8eee88fe81aac6cb59ed144ccfe2e42ed11ef983
TheOrbMaverick <enangita@gmail.com>
2024-04-03 23:44:11 +0100

    redidnt

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,35 +1,55 @@
-# Define Nginx class
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
+# Define a class for Nginx configuration
+class nginx_config {
 
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('nginx/default.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
+    # Install Nginx package
+    package { 'nginx':
+        ensure  => installed,
+    }
 
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => "Hello World!\n",
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
+    # Define Nginx service
+    service { 'nginx':
+        ensure  => running,
+        enable  => true,
+        require => Package['nginx'],
+    }
 
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
-  }
+    # Set up Nginx configuration file
+    file { '/etc/nginx/sites-available/default':
+        ensure  => file,
+        content => "
+server {
+    listen 80;
+    server_name _;
+    root /var/www/html;
+    index index.html;
+
+    location / {
+        return 200 'Hello World!';
+        add_header Content-Type text/plain;
+    }
+
+    location /redirect_me {
+        return 301 https://youtube.com/@AfroPlayerOne?si=BueiWNt5G6RIRvJe;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+        internal;
+    }
+}
+",
+        require => Package['nginx'],
+        notify  => Service['nginx'],
+    }
+
+    # Create a 404.html page
+    file { '/var/www/html/404.html':
+        ensure  => file,
+        content => 'Ceci n\'est pas une page',
+        require => Package['nginx'],
+        notify  => Service['nginx'],
+    }
 }
 
-# Define Nginx configuration template
-file { '/etc/nginx/sites-available/default':
-  ensure => file,
-  source => 'puppet:///modules/nginx/default.erb',
-}
-
-# Apply Nginx class
-include nginx
+# Apply the class
+include nginx_config
