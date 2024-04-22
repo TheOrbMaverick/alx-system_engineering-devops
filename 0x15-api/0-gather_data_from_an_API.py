8282b6425ffb91d37594b59e72fda94b8c8f2e30
TheOrbMaverick <enangita@gmail.com>
2024-04-22 20:36:34 +0100

    which indeed

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Gather data from an API """
+
+import sys
+import requests
+
+def employee_todo(employeeID):
+    """Retrieve employee TODO list progress from API"""
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employeeID)
+
+    userUrl = "https://jsonplaceholder.typicode.com/users?id={}".format(employeeID)
+    nameResponse = requests.get(userUrl)
+    name = nameResponse.json()
+    employee_name = name[0]["username"]
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Error: Unable to fetch data from API")
+        return
+    
+    todos = response.json()
+    if not todos:
+        print("No data available for employee ID:", employeeID)
+        return
+    
+    completed_tasks = 0
+    total_tasks = len(todos)
+
+    for todo in todos:
+        if todo['completed'] == True:
+            completed_tasks = completed_tasks +1
+
+    print("Employee {} is done with tasks({}/{})".format(employee_name, completed_tasks, total_tasks))
+    for todo in todos:
+        if todo['completed'] == True:
+            print("    {}".format(todo['title']))
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employeeID = sys.argv[1]
+
+    if not employeeID.isdigit():
+        print("Error: Employee ID must be an integer")
+        sys.exit(1)
+
+    employee_todo(int(employeeID))
