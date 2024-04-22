bac2f692fa746edd0ad71add78f33e037e33f7cc
TheOrbMaverick <enangita@gmail.com>
2024-04-23 00:03:37 +0100

    we dey move

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,36 +11,42 @@ if __name__ == "__main__":
 
     url = "https://jsonplaceholder.typicode.com/todos"
 
-
     response = requests.get(url)
     if response.status_code != 200:
         print("Error: Unable to fetch data from API")
-
+        sys.exit(1)
 
     todos = response.json()
     if not todos:
         print("No data available for employee ID")
-  
-    
-    for id in todos:
-        employeeID = id["userId"]
+        sys.exit(1)
 
-        userUrl = "https://jsonplaceholder.typicode.com/users?id={}".format(
-        employeeID
-        )
+    # Dictionary to store todo data for each employee
+    all_employee_data = {}
 
+    for todo in todos:
+        employeeID = todo["userId"]
+
+        # Retrieve user information for the employee
+        userUrl = f"https://jsonplaceholder.typicode.com/users/{employeeID}"
         nameResponse = requests.get(userUrl)
         name = nameResponse.json()
-        employee_name = name[employeeID]["username"]
+        employee_name = name["username"]
 
-        output_data = {str(employeeID):[]}
-        for todo in todos:
-            task_completed_status = todo['completed']
-            output_data[str(employeeID)].append({
-                "task": todo['title'],
-                "completed": task_completed_status,
-                "username": employee_name
-            })
-            output_file = "todo_all_employees.json"
-            with open(output_file, "w") as file:
-                json.dump(output_data, file, indent=4)
+        # Prepare todo data for the current employee
+        task_completed_status = todo['completed']
+        todo_data = {
+            "username": employee_name,
+            "task": todo['title'],
+            "completed": task_completed_status
+        }
+
+        # Add todo data to the list of todos for this employee
+        if employeeID not in all_employee_data:
+            all_employee_data[employeeID] = []
+        all_employee_data[employeeID].append(todo_data)
+
+    # Write all employee data to a single JSON file
+    output_file = "todo_all_employees.json"
+    with open(output_file, "w") as file:
+        json.dump(all_employee_data, file, indent=4)
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -1,1 +1,7 @@
 0x15. API
+
+# How to use these files:
+"<filename> <user_id>"
+Usage: python3 2-export_to_JSON.py 2
+
+all employees: python3 3-dictionary_of_list_of_dictionaries
