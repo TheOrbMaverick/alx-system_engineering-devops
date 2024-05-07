ba7b32a7f6fe132672ef313328063d04d5d18138
TheOrbMaverick <enangita@gmail.com>
2024-05-08 00:44:01 +0100

    reassign

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,13 +19,3 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        num_subscribers = number_of_subscribers(subreddit)
-        print(num_subscribers)
