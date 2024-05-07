989cd98413bc94d2f556660e9f7dc8968dd8da7b
TheOrbMaverick <enangita@gmail.com>
2024-05-07 23:07:04 +0100

    reignite

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    headers = {"user-Agent:" "My User Agent 007"}
+    headers = {"user-Agent": "My User Agent 007"}
 
     response = requests.get(url, headers=headers)
 
