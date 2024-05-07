db14b319b393667f2bf6bff229e699a46102eabd
TheOrbMaverick <enangita@gmail.com>
2024-05-07 23:58:56 +0100

    reassign

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ Function that queries the Reddit API and
 returns the number of subscribers
 """
 
-
+import sys
 import requests
 
 
@@ -13,11 +13,11 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API
     """
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    # headers = {'User-Agent': 'My User Agent 1.0'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
 
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         data = response.json()
         subscribers = data['data']['subscribers']
@@ -26,3 +26,11 @@ def number_of_subscribers(subreddit):
         subscribers = 0
 
     return subscribers
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        print(number_of_subscribers(subreddit))
