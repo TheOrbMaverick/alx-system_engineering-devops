2be2aa72f922e5806ac5477e0f9bfefe9828d7d0
TheOrbMaverick <enangita@gmail.com>
2024-05-07 22:37:50 +0100

    redoing

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and
+returns the number of subscribers
+"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {"user-Agent:" "My User Agent 007"}
+
+    response = requests.get(url, headers=headers)
+
+    returned_data = response.json()
+
+    if "data" in returned_data and "subscribers" in returned_data["data"]:
+        return returned_data["data"]["subscribers"]
+    else:
+        return (0)
