8d36e23d77516e15bf97bc0a93a1f45fde6349b1
TheOrbMaverick <enangita@gmail.com>
2024-05-08 21:22:13 +0100

    puppet for wordpress fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,6 @@
 # Fix Apache configuration to resolve 500 error
 
-# Ensure Apache package is installed
-package { 'apache2':
-  ensure => 'installed',
-}
-
-# Ensure Apache service is running and enabled
-service { 'apache2':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['apache2'],
-}
-
-# Manage Apache configuration file (e.g., /etc/apache2/apache2.conf)
-file { '/etc/apache2/apache2.conf':
-  ensure  => file,
-  content => template('apache/apache2.conf.erb'), # Use a template to manage configuration content
-  notify  => Service['apache2'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/usr/local/bin', '/bin'],
 }
