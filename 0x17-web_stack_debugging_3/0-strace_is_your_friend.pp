7bc0b90040bbe8c168211117617597c1c6b7fe30
TheOrbMaverick <enangita@gmail.com>
2024-05-08 20:27:45 +0100

    my re era

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,16 @@
 # Fix Apache configuration or permissions issue
 file { '/path/to/affected/file':
   ensure => 'file',
-  owner  => 'apache',
-  group  => 'apache',
+  owner  => 'www-data',
+  group  => 'www-data',
   mode   => '0644',
   content => template('apache/affected_file.erb'),
 }
 
 # Ensure Apache service is restarted after the configuration change
 service { 'apache2':
-  ensure     => 'running',
-  enable     => true,
-  subscribe  => File['/path/to/affected/file'],
+  ensure    => 'running',
+  enable    => true,
+  provider  => 'init',
+  subscribe => File['/path/to/affected/file'],
 }
